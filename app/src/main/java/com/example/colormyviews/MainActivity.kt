7ef9ada3f1ee806a949beaeaf0f34a355e360781
liowxe99 <liowxe-wm17@student.tarc.edu.my>
2019-11-03 06:18:52 +0800

    last

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.colormyviews
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
 import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var mySizeList : List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
     }
+    @SuppressLint("ResourceType")
     private fun makeColored(view: View) {
+
         when (view.id) {
 
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_one)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_three)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_four)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_five)
         }
     }
     private fun setListeners() {
@@ -31,7 +35,10 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
-
+        mySizeList = listOf(
+            boxOneText.maxHeight,boxTwoText.maxHeight, boxThreeText.maxHeight,
+            boxFourText.maxHeight, boxFiveText.maxHeight
+        )
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
         val clickableViews: List<View> =
             listOf(
@@ -40,7 +47,9 @@ class MainActivity : AppCompatActivity() {
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
+
         }
+
 
     }
 }
