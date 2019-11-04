d98397d1eed11f9903e3bba52b62b15b37ad7fb0
liowxe99 <liowxe-wm17@student.tarc.edu.my>
2019-11-04 19:00:53 +0800

    last

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.colormyviews
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Size
 import android.view.View
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
-    lateinit var mySizeList : List<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     }
     @SuppressLint("ResourceType")
     private fun makeColored(view: View) {
-
         when (view.id) {
 
             // Boxes using Color class colors for the background
@@ -26,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_three)
             R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_four)
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_five)
+            R.id.constraint_layout -> view.setBackgroundColor(0xFF00FF00.toInt())
         }
     }
     private fun setListeners() {
@@ -35,10 +34,6 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
-        mySizeList = listOf(
-            boxOneText.maxHeight,boxTwoText.maxHeight, boxThreeText.maxHeight,
-            boxFourText.maxHeight, boxFiveText.maxHeight
-        )
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
         val clickableViews: List<View> =
             listOf(
