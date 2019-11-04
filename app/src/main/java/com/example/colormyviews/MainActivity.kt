e4bbfef06195ca8eea9d6231a5f1fbbf1d5ad797
liowxe99 <liowxe-wm17@student.tarc.edu.my>
2019-11-04 19:27:57 +0800

    last

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.colormyviews
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +21,16 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
 
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_one)
-            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
-            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_three)
-            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_four)
-            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_five)
-            R.id.constraint_layout -> view.setBackgroundColor(0xFF00FF00.toInt())
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
     private fun setListeners() {
@@ -35,10 +41,14 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
